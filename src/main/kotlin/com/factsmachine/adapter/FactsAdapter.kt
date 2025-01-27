d05214e996b8c8aa5efb9b11b1f6ba2b1c060c8d
Konstantin Safutin <konstantin.safutin@zalando.de>
2025-01-27 23:24:31 +0100

    Http client config refactoring

--- src/main/kotlin/com/factsmachine/adapter/FactsAdapter.kt
+++ src/main/kotlin/com/factsmachine/adapter/FactsAdapter.kt
@@ -3,7 +3,6 @@ package com.factsmachine.adapter
 import com.factsmachine.adapter.dto.FactDto
 import io.ktor.client.*
 import io.ktor.client.call.*
-import io.ktor.client.plugins.*
 import io.ktor.client.request.*
 
 interface FactsAdapter {
@@ -15,7 +14,6 @@ class UselessFactsAdapter(private val apiClient: HttpClient, private val url: St
     override suspend fun getFact(): FactDto {
         val httpResponse =  apiClient.get(url) {
             parameter("language", "en")
-            expectSuccess = true
         }
         return httpResponse.body<FactDto>()
     }
--- src/main/kotlin/com/factsmachine/config/DIBootstrap.kt
+++ src/main/kotlin/com/factsmachine/config/DIBootstrap.kt
@@ -9,11 +9,15 @@ import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.client.plugins.contentnegotiation.*
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
+const val USELESS_FACTS_CLIENT = "UselessFactsClient"
+
 fun Application.configureDI() {
+
     val uselessFactsUrl = environment.config.property("ktor.uselessfactsservice.url").getString()
     val baseUrl = environment.config.property("ktor.baseurl").getString()
 
@@ -22,11 +26,12 @@ fun Application.configureDI() {
         modules(module {
             single<FactsService> { FactsServiceImpl(get(), get(), get(), baseUrl) }
             single<StorageService> { InMemoryStorageService() }
-            single<FactsAdapter> { UselessFactsAdapter(get(), uselessFactsUrl) }
+            single<FactsAdapter> { UselessFactsAdapter(get(named(USELESS_FACTS_CLIENT)), uselessFactsUrl) }
             single<StatisticsService> { StatisticsServiceImpl(get(), baseUrl) }
             single<IdGeneratorService> { Base62IdGeneratorService() }
-            single {
+            single<HttpClient>(named(USELESS_FACTS_CLIENT)) {
                 HttpClient(OkHttp) {
+                    expectSuccess = true
                     install(ContentNegotiation) {
                         jackson {
                             configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
